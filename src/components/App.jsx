6152332c4a5b822ab56b01b10b80af0f3ad26e99
janardhan <janardhan.reddy75@gmail.com>
2020-12-03 11:40:51 +0000

    second change for errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import Navigation from './navigation';
 import Header from './header';
-import Features from './features';
+//import Features from './features';
 import About from './about';
 import Services from './services';
 import Gallery from './gallery';
-import Testimonials from './testimonials';
+//import Testimonials from './testimonials';
 import Team from './Team';
 import Contact from './contact';
 import $ from 'jquery';
