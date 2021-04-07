13238312d0eb762e9fe7e3b636c1eb37528aeee1
janardhan904 <janardhan.reddy75@gmail.com>
2021-04-07 18:07:43 +0100

    adding Linkedin

--- src/components/newjobs.jsx
+++ src/components/newjobs.jsx
@@ -5,28 +5,21 @@ export class NewJobs extends Component {
   render() {
     return (
       <div id="newjobsport" className="text-center">
+        <div className="container">
         <div className="section-title">
-        <div className="newjobscontainer">
         <p className="avail">
         <h2 >Available Opportunities</h2>
         <div className="listofop">
-        <span>DevOps Engineer(s)</span>
+          <h4>
+        <p>Excited to join our journey?  <br/><br/> Please look out for new Opportunities at our LinkedIn New jobs section.
+        <a href="https://www.linkedin.com/company/smart-machine-solutions/jobs/">Click Here.</a>
+        
+        </p>
+        </h4>
         <br/>
-        <span>Software Developer(s)</span>
-        <br/>
-        <span>UI Developer</span>
-        <br/>
-        <span>SAP Consultant</span>
-        <br/>
-        <span>Solution Architect</span>
-        <br/>
-        <span>IT Consultant</span>
         <br/>
         </div>
         </p>
-        <a className="JobLinkDoc" href="https://drive.google.com/file/d/1YYSfJmU5yZKHiXcppTPbPfjM6rwa3Qux/view?usp=sharing" target="_blank" rel="noopener noreferrer">Click to see Job Descriptions</a>
-        <br/>
-        <span>Email: hr@smartmachinesolutions.co.uk</span>
         </div>
         </div>
         </div>
