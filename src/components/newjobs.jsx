b29c354e168ec37bccd15f879c74a7c780a02d56
janardhan904 <janardhan.reddy75@gmail.com>
2021-07-27 05:19:15 +0100

    done with data

--- src/components/newjobs.jsx
+++ src/components/newjobs.jsx
@@ -7,6 +7,9 @@ export class NewJobs extends Component {
     this.state = {
       show: false,
       show1: false,
+      show2: false,
+      show3: false,
+      show4: false,
       close: false,
     };
   }
@@ -172,6 +175,257 @@ export class NewJobs extends Component {
                         >
                           <h3>Job Description:</h3>
                           <li>
+                          Big Data integration to Tableau using SQL database query to automate executive-level dashboards. 
+                          </li>
+                          <li>
+                          BigData integration of OSS measurement into Quality standards and Quality Management Systems 
+                          </li>
+                          <li>
+                          Single Point of Contact for Customer experience management bigdata platform deployment, installation, commissioning, maintenance, administration and deliver contractual reports. 
+                          </li>
+                          <li>
+                          Design and implement data module using ETL process and tools. 
+                          </li>
+                          Deploy CEM tool – SPI (module of SmartCare), from integration to commissioning, maintenance, and administration of the platform. 
+                          <li>
+                          Develop processes with Python programming to tracking new site progress and change requests. 
+                          </li>
+                          R language for automation – Improving reliability with reporting & efficiency in resource management.
+                          <li>
+                          Excellent management skills, maintain project plans and deadline. 
+                          </li>
+                        </ul>
+                        <ul
+                          class="entry-content"
+                          style={{ listStyleType: "unset" }}
+                        >
+                          <h3>Skills and Qualifications:</h3>
+                          <li>
+                          Master's Degree in the related field with minimum 8 years experience. {" "}
+                          </li>
+                          <li>
+                          Proven experience in an advanced level of Tableau, Power BI, Geocoding API.{" "}
+                          </li>
+                          <li>
+                          Hands-on with Python coding and demonstrated experience in libraries such as - Pandas, NumPy, GeoPandas, OS, Pyodbc, etc.{" "}
+                          </li>
+
+                          <li>
+                          Sound understanding of data science techniques and Excellent knowledge of tools such as SAP Business 
+                          Objects XI, OPTIMA, Advance MS-Office, Toad, VB macros.{" "}
+                          </li>
+                        </ul>
+                      </Modal.Body>
+                      <Modal.Footer className="py-1 d-flex justify-content-center">
+                        <div>
+                          <Button
+                            variant="outline-dark"
+                            onClick={() => this.setState({ show1: false })}
+                          >
+                            Close
+                          </Button>
+                        </div>
+                      </Modal.Footer>
+                    </Modal>
+
+
+                    <tr
+                      style={{ cursor: "hand" }}
+                      onClick={() => this.setState({ show2: true })}
+                    >
+                      <th scope="row">3</th>
+                      <td>Business Analyst</td>
+                      <td>SMS21-BA-1</td>
+                      <td>Closed</td>
+                      <td>26/04/2021</td>
+                      <td>21/05/2021</td>
+                      <td>Sponsorship Avialable</td>
+                    </tr>
+                    <Modal
+                      show={this.state.show2}
+                      animation={true}
+                      size="lg"
+                      className=" shadow-lg border"
+                    >
+                      <Modal.Header className="text-info text-white text-center py-1">
+                        <Modal.Title className="text-center">
+                          <h2>JOB DESCRIPTION</h2>
+                        </Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body className="py-0 border">
+                        <ul
+                          class="entry-content"
+                          style={{ listStyleType: "unset" }}
+                        >
+                          <h3>Job Description:</h3>
+                          <li>
+                          Identify problems concerned with business strategy, policy, procedures, methods, and markets of the organisation seeking advice. 
+                          </li>
+                          <li>
+                          Carry out Gap Analysis between new Business Requirements and Existing Capabilities. 
+                          </li>
+                          <li>
+                          Determine the appropriate method of data collection and research methodology, analyse, and interpret information gained and formulate and implement recommendations and solutions. 
+                          </li>
+                          <li>
+                          Communicate and represent complex information in easily understandable visual representations with excellent written and presentation skills.
+                          </li>
+                          Examine existing business models and flows of data and
+                          design functional specifications and test plans for
+                          new systems to meet client’s needs.
+                          <li>
+                          Create supporting documentation using appropriate Business Analysis Techniques.
+                          </li>
+                        </ul>
+                        <ul
+                          class="entry-content"
+                          style={{ listStyleType: "unset" }}
+                        >
+                          <h3>Skills and Qualifications:</h3>
+                          <li>
+                          Proven Experience as a Business Analyst {" "}
+                          </li>
+                          <li>
+                          Ability to demonstrate experience of various Business Analysis Techniques.{" "}
+                          </li>
+                          <li>
+                          Excellent written, verbal and presentation skills.{" "}
+                          </li>
+
+                          <li>
+                          Proficient in the use of Word, Excel, PowerPoint, and Visio and Salesforce experience would be a great advantage{" "}
+                          </li>
+                        </ul>
+                      </Modal.Body>
+                      <Modal.Footer className="py-1 d-flex justify-content-center">
+                        <div>
+                          <Button
+                            variant="outline-dark"
+                            onClick={() => this.setState({ show2: false })}
+                          >
+                            Close
+                          </Button>
+                        </div>
+                      </Modal.Footer>
+                    </Modal>
+
+
+                    <tr
+                      style={{ cursor: "hand" }}
+                      onClick={() => this.setState({ show3: true })}
+                    >
+                      <th scope="row">4</th>
+                      <td>Software Tester</td>
+                      <td>SMS21-ST-1</td>
+                      <td>Closed</td>
+                      <td>26/04/2021</td>
+                      <td>21/05/2021</td>
+                      <td>Sponsorship Avialable</td>
+                    </tr>
+                    <Modal
+                      show={this.state.show3}
+                      animation={true}
+                      size="lg"
+                      className=" shadow-lg border"
+                    >
+                      <Modal.Header className="text-info text-white text-center py-1">
+                        <Modal.Title className="text-center">
+                          <h2>JOB DESCRIPTION</h2>
+                        </Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body className="py-0 border">
+                        <ul
+                          class="entry-content"
+                          style={{ listStyleType: "unset" }}
+                        >
+                          <h3>Job Description:</h3>
+                          <li>
+                          Execute all levels of testing i.e., Functional, System, Integration, and Regression.
+                          </li>
+                          <li>
+                          Design and develop automation scripts, test plans and test cases. 
+                          </li>
+                          <li>
+                          Implement JUnit for different modules of Java-based web applications. 
+                          </li>
+                          <li>
+                          Perform functional, unit and performance testing of the entire product and application. 
+                          </li>
+                          Use version control tools such as CVS and subversion and Provide timely solutions.
+                          <li>
+                          Apply quality engineering principles throughout the Agile/Scrum product lifecycle.
+                          </li>
+                          Work closely with clients to implement new systems.
+                          <li>
+                          Provide support and documentation. 
+                          </li>
+                        </ul>
+                        <ul
+                          class="entry-content"
+                          style={{ listStyleType: "unset" }}
+                        >
+                          <h3>Skills and Qualifications:</h3>
+                          <li>
+                          6+ years' experience as a software tester.{" "}
+                          </li>
+                          <li>
+                          Knowledge of Software development life cycle and software test life cycle.{" "}
+                          </li>
+                          <li>
+                          Hands-on experience with automation testing tools Eclipse, Selenium and Junit.{" "}
+                          </li>
+
+                          <li>
+                          Knowledge of test build tools such as Maven and Jenkins. {" "}
+                          </li>
+                          <li>
+                          Knowledge of Java programming knowledge would be advantageous.
+                          </li>
+                        </ul>
+                      </Modal.Body>
+                      <Modal.Footer className="py-1 d-flex justify-content-center">
+                        <div>
+                          <Button
+                            variant="outline-dark"
+                            onClick={() => this.setState({ show3: false })}
+                          >
+                            Close
+                          </Button>
+                        </div>
+                      </Modal.Footer>
+                    </Modal>
+
+
+                    <tr
+                      style={{ cursor: "hand" }}
+                      onClick={() => this.setState({ show4: true })}
+                    >
+                      <th scope="row">5</th>
+                      <td>IT Consultant</td>
+                      <td>SMS21-ITC-2</td>
+                      <td>Closed</td>
+                      <td>03/05/2021</td>
+                      <td>28/05/2021</td>
+                      <td>Sponsorship Avialable</td>
+                    </tr>
+                    <Modal
+                      show={this.state.show4}
+                      animation={true}
+                      size="lg"
+                      className=" shadow-lg border"
+                    >
+                      <Modal.Header className="text-info text-white text-center py-1">
+                        <Modal.Title className="text-center">
+                          <h2>JOB DESCRIPTION</h2>
+                        </Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body className="py-0 border">
+                        <ul
+                          class="entry-content"
+                          style={{ listStyleType: "unset" }}
+                        >
+                          <h3>Job Description:</h3>
+                          <li>
                             Liaise with internal/external clients to analyse
                             business procedure, clarify client’s requirements
                             and to define the scope of existing software,
@@ -223,7 +477,7 @@ export class NewJobs extends Component {
                           </li>
 
                           <li>
-                            6+ years’ experience in the IT Infrastructure
+                            4+ years’ experience in the IT Infrastructure
                             management.{" "}
                           </li>
                         </ul>
@@ -232,7 +486,7 @@ export class NewJobs extends Component {
                         <div>
                           <Button
                             variant="outline-dark"
-                            onClick={() => this.setState({ show1: false })}
+                            onClick={() => this.setState({ show4: false })}
                           >
                             Close
                           </Button>
@@ -241,42 +495,6 @@ export class NewJobs extends Component {
                     </Modal>
 
 
-                    <tr
-                      style={{ cursor: "hand" }}
-                      onClick={() => this.setState({ show1: true })}
-                    >
-                      <th scope="row">3</th>
-                      <td>Business Analyst</td>
-                      <td>SMS21-BA-1</td>
-                      <td>Closed</td>
-                      <td>26/04/2021</td>
-                      <td>21/05/2021</td>
-                      <td>Sponsorship Avialable</td>
-                    </tr>
-                    <tr
-                      style={{ cursor: "hand" }}
-                      onClick={() => this.setState({ show: true })}
-                    >
-                      <th scope="row">4</th>
-                      <td>Software Tester</td>
-                      <td>SMS21-ST-1</td>
-                      <td>Closed</td>
-                      <td>26/04/2021</td>
-                      <td>21/05/2021</td>
-                      <td>Sponsorship Avialable</td>
-                    </tr>
-                    <tr
-                      style={{ cursor: "hand" }}
-                      onClick={() => this.setState({ show: true })}
-                    >
-                      <th scope="row">5</th>
-                      <td>IT Consultant</td>
-                      <td>SMS21-ITC-2</td>
-                      <td>Closed</td>
-                      <td>03/05/2021</td>
-                      <td>28/05/2021</td>
-                      <td>Sponsorship Avialable</td>
-                    </tr>
                   </tbody>
                 </table>
               </div>
